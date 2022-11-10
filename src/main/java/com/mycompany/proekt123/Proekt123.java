5b46e5367924f4ab5838eb5c1c526761ed32b3d9
Anna <prozorovaana9@gmail.com>
2022-11-10 19:18:09 +0700

    dytcty

--- src/main/java/com/mycompany/proekt123/Proekt123.java
+++ src/main/java/com/mycompany/proekt123/Proekt123.java
@@ -11,7 +11,34 @@ package com.mycompany.proekt123;
  */
 public class Proekt123 {
 
+//  pripublic classvate boolean isPrime(int n, List<Integer> primes) {
+    
+public class 
+ if(n < 2)
+   return null;
+ boolean []sieve=boolean[n+1];
+ int p=2;
+ for (p=2;p<n/2;p++)
+ 
+ 
+ 
+ for(int i=p)
+        public
+for (var i = 0; i < primes.size(); i++) {
+        var prime = primes.get(i);
+        if (prime * prime > n) {
+            return true;
+        }
+        if (n % prime == 0) {
+            return false;
+        }
+    }
+    return true;
+} 
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
+        
+  
+  }
 }
